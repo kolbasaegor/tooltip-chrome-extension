72e4a33c6a0ab46abb11480b513a264d398c1a1d
Egor Stroganov <xxxstroganov@gmail.com>
2023-06-03 00:43:07 +0300

    refactoring & added new functionality

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,11 +1,4 @@
-// const addStatus = async (hasTooltips) => {
-//     const status = document.querySelector("#has-tooltips");
-
-//     status.textContent = hasTooltips ?
-//     "✅ Для этой страницы есть подсказки" :
-//     "❌ Для этой страницы нет подсказок";
-// }
-
+const content = document.querySelector(".content");
 
 const getCurrentTabUrl = async () => {
     const currentTab = await chrome.tabs.query({ active: true, currentWindow: true });
@@ -16,93 +9,6 @@ const getOrigin = async () => {
     const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
     return new URL(tabs[0].url).origin;
 }
-
-
-// const isTooltipsEnabledForThisSite = async () => {
-//     const origin = await getOrigin();
-
-//     chrome.runtime.sendMessage({
-//         dest: "service",
-//         from: "popup",
-//         query: "isTooltipsEnabledForThisSite?",
-//         url: origin
-//     });
-// }
-
-// const isTooltipsEnabledForThisUrl = async () => {
-//     const url = await getCurrentTabUrl();
-
-//     chrome.runtime.sendMessage({
-//         dest: "service",
-//         from: "popup",
-//         query: "isTooltipsEnabledForThisUrl?",
-//         url: url
-//     });
-// }
-
-// const isTooltips_site = async () => {
-//     const origin = await getOrigin();
-
-//     const response = await chrome.runtime.sendMessage({
-//         dest: "service",
-//         from: "popup",
-//         query: "anyTooltipsForThisSite?",
-//         url: origin
-//     });
-
-//     return response;
-// }
-
-// const isTooltips_url = async () => {
-//     const currentTabUrl = await getCurrentTabUrl();
-
-//     const response = await chrome.runtime.sendMessage({
-//         dest: "service",
-//         from: "popup",
-//         query: "anyTooltipsForThisUrl?",
-//         url: currentTabUrl
-//     });
-
-//     return response;
-// }
-
-// const setCookies = async (url, newValue) => {
-//     chrome.runtime.sendMessage({
-//         dest: "service",
-//         from: "popup",
-//         query: "setCookie",
-//         url: url,
-//         newValue: newValue ? "1" : "0"
-//     });
-// }
-
-// const createTooltipsCheckbox = (status) => {
-//     const checkbox = document.createElement('input');
-//     checkbox.className = "checkbox";
-//     checkbox.type = 'checkbox';
-//     checkbox.checked = status;
-
-//     const checkboxLine = document.createElement("div");
-//     checkboxLine.className = "container entry";
-
-//     const title = document.createElement("p");
-//     title.textContent = "Show tooltips on this website";
-
-//     checkboxLine.appendChild(title);
-//     checkboxLine.appendChild(checkbox);
-
-//     const content = document.querySelector(".content");
-//     content.appendChild(checkboxLine);
-
-//     checkbox.addEventListener('change', async (event) => {
-//         const status = event.target.checked;
-//         const origin = await getOrigin();
-
-//         setCookies(origin, status);
-//     });
-// }
-
-const content = document.querySelector(".content");
 
 const firstLine = async (status) => {
     const statusElem = document.createElement("div");
